8bff663b9fea1c2138569ae626acdd3f82663b06
MarkusH <markheik@edu.oulu.fi>
2019-07-21 18:12:33 +0300

    big changes, user db class, routing, namespace, structure

--- scr/app.py
+++ scr/app.py
@@ -1,14 +1,10 @@
 import click
 import json
 from flask import Flask, Blueprint, request, Response
-from flask_restplus import Resource, Api, Namespace
+from flask_restplus import Resource, Api, Namespace, fields
 from flask_sqlalchemy import SQLAlchemy
+from models.models import create_app, UserItem, db
 
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///development.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
 blueprint = Blueprint('api', __name__, url_prefix="/api")
 api = Api(
@@ -17,31 +13,44 @@ api = Api(
     version='0.1',
     description='Save money by using this app!'
 )
+
+app = create_app()
 app.register_blueprint(blueprint)
+app.app_context().push()
+
+user = api.namespace(name='users', description='User controls')
+api.add_namespace(user)
+
 MIMETYPE = "application/json"
-asd = api.namespace(name='users', description='User controls')
-api.add_namespace(asd)
+
+user_model = api.model('User model', {
+    'name': fields.String
+})
 
 
-@asd.route('/<string:user>/')
+@user.route('/<string:user>/')
+@user.param('user', 'The username')
 class User(Resource):
-    def get(self):
+    def get(self, user):
         resp = {
-            'user': "Markus",
+            'user': user,
             'balance': 1002
         }
         return Response(json.dumps(resp), 200, mimetype=MIMETYPE)
 
 
-@asd.route('/')
+@user.route('/')
 class User(Resource):
-    def get(self):
-        resp = {
-            'user': "Markus",
-            'balance': 1002
-        }
-        return Response(json.dumps(resp), 200, mimetype=MIMETYPE)
+    @api.expect(user_model)
+    def post(self):
+        user = UserItem(
+            name=request.json['name']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return Response(status=201, mimetype=MIMETYPE)
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
---
+++ scr/models/models.py
@@ -0,0 +1,19 @@
+import click
+import json
+from flask import Flask, Blueprint, request, Response
+from flask_restplus import Resource, Api, Namespace
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///development.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    return app
+
+
+class UserItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
