fd6c2a45d33b954cb20fb3cf7c18c48dbddae6b5
MarkusH <markheik@edu.oulu.fi>
2019-07-24 16:11:40 +0300

    user not found error handling, error utils

--- scr/models/users/usermodels.py
+++ scr/models/users/usermodels.py
@@ -3,47 +3,52 @@ from flask_restplus import Resource, Api, Namespace, fields
 import json
 from models.models import db
 from models import api, blueprint
+from utils.error import create_error_response, error_model
 
 
 users = Namespace(name='Users', description='User controls')
 
 user_model = api.model('User model', {
-        'name': fields.String(example='model user', description='Users name'),
-        'balance': fields.Float(example=133, description='Account balance in euros.')
+        'user': fields.String(example='model user', description='Username'),
+        'balance': fields.Float(example=133, description='Account balance in euros')
 })
-
 
 MIMETYPE = "application/json"
 
 
 class UserItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    user = db.Column(db.String, unique=True, nullable=False)
     balance = db.Column(db.Float, nullable=False)
 
 
 @users.route('/<string:user>/')
-@users.param('user', 'Account name')
+@users.param('user', 'Account user')
+@api.response(404, 'Not found', error_model)
 class User(Resource):
     def get(self, user):
+        db_user = UserItem.query.filter_by(user=user).first()
+        if db_user is None:
+            return create_error_response(404, "Not found", f'User: {user} was not found')
         resp = {
-            'user': user,
-            'balance': 1002
+            'user': db_user.user,
+            'balance': db_user.balance
         }
         return Response(json.dumps(resp), 200, mimetype=MIMETYPE)
 
 
 @users.route('/')
+@api.response(201, 'Created', headers={"self": '/api/users/<user>/'})
 class User1(Resource):
     @users.expect(user_model)
     def post(self):
         user = UserItem(
-            name=request.json['name'],
+            user=request.json['user'],
             balance=request.json['balance']
         )
         db.session.add(user)
         db.session.commit()
-        uri = api.url_for(User, user=request.json['name'])
+        uri = api.url_for(User, user=request.json['user'])
         return Response(
             status=201,
             mimetype=MIMETYPE,
